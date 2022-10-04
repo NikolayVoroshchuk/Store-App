4b7767503cd47945f7f64f0d11241023bf4e39b5
Николай Ворощук <nikolay.voroshchuk98@gmail.com>
2022-10-04 12:56:17 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import { store } from './store/store';
 import './index.css';
@@ -11,8 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
 );
